53eeaba37b8de5e0825c203d9057d0a9b837118f
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-03-20 16:33:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Import stylesheets
+import './style.css';
 // Complete the js code
 
 const limit = 50; // Heap memory usage limit in MB
@@ -6,18 +8,41 @@ let elements = []; // Array to hold generated DOM elements
 // Function to generate DOM elements
 const generateElements = () => {
   // complete the function
+
+	for(let i = 0 ; i< 100;i++){
+
+		const divEle = document.createElement("div");
+		divEle.className="element";
+		divEle.textContent = `Items ${elements.length +1}`;
+
+		document.getElementById("container").appendChild(divEle);
+		elements.push(divEle);
+	}
+	
   updateMemoryUsage();
 };
 
 // Function to remove DOM elements
 const removeElements = () => {
   // complete the function
+  elements.forEach(el=> el.remove());
+  elements = [];
   updateMemoryUsage();
 };
 
 // Function to update memory usage display
 const updateMemoryUsage = () => {
   // Complete this function
+  if (performance.memory) {
+    let usedMemory = (performance.memory.usedJSHeapSize / 1024 / 1024).toFixed(2);
+    document.getElementById("memory").textContent = `Memory Usage: ${usedMemory} MB`;
+
+    if (usedMemory > limit) {
+        alert("Memory limit exceeded! Please remove elements.");
+    }
+} else {
+    document.getElementById("memory").textContent = `Memory Usage: Not supported in this browser`;
+}
 };
 
 // Add event listeners to buttons
